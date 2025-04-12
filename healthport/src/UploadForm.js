a8a9f1232bad0a84063d6b31c48dbb81e9c361d5
Dan Pham <danpham0312@gmail.com>
2025-04-11 22:21:45 -0700

    commit

--- healthport/src/UploadForm.js
+++ healthport/src/UploadForm.js
@@ -5,7 +5,18 @@ const uploadFile = () =>{
     // creating a state to hold the uploaded files, could change this later
     const [files, setFiles] = useState([]);
 
+    // dropzone hook: functions to define behavior of dropzone, and file input
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: '.pdf, image/*',
+        onDrop: (acceptedFiles) => { // gets called when files are dropped
+            //console.log(acceptedFiles);
+            setFiles(acceptedFiles.map(file => Object.assign(file, {preview: URL.createObjectURL(file)})));
+        }
+    });
+
     
 }
+
+// going to do basic one first, no display preview yet
 
 // document progress: styling, parse contents or upload file itself, submit button, backend server
